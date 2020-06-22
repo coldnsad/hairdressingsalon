a7d8ea23869adc242b55d0387a66af3ece6e6100
Vladislav <50295128+vadya9605@users.noreply.github.com>
2020-06-22 11:41:59 +0700

    addtogithub

---
+++ enter.php
@@ -0,0 +1,12 @@
+<?php
+echo <<<_TEST
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <form method='POST' action='enter.php'>
+                Test<input type='text'> 
+            </form>
+        </body>
+    </html>
+_TEST
+?>
---
+++ redirect.js
@@ -0,0 +1,4 @@
+function redirect()
+{
+    window.location = 'http://localhost/HairdressingSalon/enter.php';
+}
